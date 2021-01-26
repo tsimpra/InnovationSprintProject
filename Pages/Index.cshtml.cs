9172882617aa6f870288febdd03f0f5749e690fd
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-01-26 19:50:18 +0200

    Added Services Project, Added createMorbidityGroup functionality + test value on app initialization.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Services;
+using EFDataAccessLibrary.DataAccess;
 
 namespace InnovationSprintProject.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly MyAppContext _db;
+        private readonly CreateServices cs = new CreateServices();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,MyAppContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
-
+            cs.db = _db;
+            cs.createMorbidityGroup("test1");
         }
     }
 }
