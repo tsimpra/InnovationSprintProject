5d91510ac4629d2c548bb19f7a60bd0c420ecce0
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-01-28 18:45:18 +0200

    Added Create Symptom Functionality.

--- InnovationSprintProject/Pages/Index.cshtml.cs
+++ InnovationSprintProject/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Services;
 using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
 
 namespace InnovationSprintProject.Pages
 {
@@ -24,12 +25,16 @@ namespace InnovationSprintProject.Pages
 
         public void OnGet()
         {
-            cs.db = _db;
-            cs.createMorbidityGroup("test1");
-            cs.createMorbidityGroup("test2");
-            var temp = _db.MorbidityGroups.ToList();
-            cs.createPatient("Test User", DateTime.Today, temp);
-
+            if (_db.MorbidityGroups.Count() == 0)
+            {
+                cs.db = _db;
+                cs.createMorbidityGroup("test1");
+                cs.createMorbidityGroup("test2");
+                var temp = _db.MorbidityGroups.ToList();
+                cs.createPatient("Test User", DateTime.Today, temp);
+                Patient patient = _db.Patients.Where(x => x.Name == "Test User").Single();
+                cs.createSymptom("Headache", DateTime.Today, patient.Id);
+            }
         }
     }
 }
