9c6a8d36345dfed3f1cb0cb8f9adc2bef9cf9f80
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-01-28 18:29:16 +0200

    Added code to create morbidity groups,patient on index.cshtml.cs .

--- InnovationSprintProject/Pages/Index.cshtml.cs
+++ InnovationSprintProject/Pages/Index.cshtml.cs
@@ -5,20 +5,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Services;
+using EFDataAccessLibrary.DataAccess;
 
 namespace InnovationSprintProject.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly MyAppContext _db;
+        private readonly CreateServices cs = new CreateServices();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, MyAppContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
+            cs.db = _db;
+            cs.createMorbidityGroup("test1");
+            cs.createMorbidityGroup("test2");
+            var temp = _db.MorbidityGroups.ToList();
+            cs.createPatient("Test User", DateTime.Today, temp);
 
         }
     }
