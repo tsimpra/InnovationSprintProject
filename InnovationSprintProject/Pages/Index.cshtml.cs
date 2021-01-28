a6adbb2c431bb3ef03e3cf2ffad937dfd653521c
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2021-01-28 20:46:22 +0200

    Updated Initialization values. Added FetchServices. Created functionality for getPatientsOlderThan and getSymptomInstancesByPatientID.

--- InnovationSprintProject/Pages/Index.cshtml.cs
+++ InnovationSprintProject/Pages/Index.cshtml.cs
@@ -16,25 +16,85 @@ namespace InnovationSprintProject.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly MyAppContext _db;
         private readonly CreateServices cs = new CreateServices();
+        private readonly FetchServices fs = new FetchServices();
 
         public IndexModel(ILogger<IndexModel> logger, MyAppContext db)
         {
             _logger = logger;
             _db = db;
+            cs.db = db;
+            fs.db = db;
         }
 
         public void OnGet()
         {
             if (_db.MorbidityGroups.Count() == 0)
             {
-                cs.db = _db;
-                cs.createMorbidityGroup("test1");
-                cs.createMorbidityGroup("test2");
-                var temp = _db.MorbidityGroups.ToList();
-                cs.createPatient("Test User", DateTime.Today, temp);
-                Patient patient = _db.Patients.Where(x => x.Name == "Test User").Single();
-                cs.createSymptom("Headache", DateTime.Today, patient.Id);
+                for (int i = 0; i < 4; i++)
+                {
+                    cs.createMorbidityGroup("MorbidityGroup " + i);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    cs.createPatient("Patient " + i, generateDateOfBirth(), generateMorbidityGroups());
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Patient patient = _db.Patients.Where(x => x.Name == ("Patient "+i)).Single();
+                    cs.createSymptom("Symptom "+i, generateDateOfOccurance(), patient.Id);
+                }                
             }
+
+            //printing the patients older than 30 years old to the console
+            fs.getPatientsOlderThan(30).ForEach(x => Console.Out.WriteLine(x.Name));
+
+            //printing the symprtom instances of the "Patient 5" patient
+            Patient pat = _db.Patients.Where(x => x.Name == ("Patient " + 5)).Single();
+            cs.createSymptom("Another Symptom", generateDateOfOccurance(), pat.Id); //adding another symptom instance to "Patient 5" patient
+            fs.getSymptomInstancesByPatientID(pat.Id).ForEach(x=> Console.Out.WriteLine(x.Name));
+
+            
+
         }
+
+        //choosing 2 morbidity groups randomly from the database
+        private List<MorbidityGroup> generateMorbidityGroups()
+        {
+            var groups = _db.MorbidityGroups.ToList();
+            Random gen = new Random();
+            int range = _db.MorbidityGroups.Count();
+            int num = gen.Next(range);
+            List<MorbidityGroup> temp = new List<MorbidityGroup>();
+            temp.Add(_db.MorbidityGroups.Where(x => x.Name == ("MorbidityGroup " + num)).Single());
+            num = gen.Next(range);
+            temp.Add(_db.MorbidityGroups.Where(x => x.Name == ("MorbidityGroup " + num)).Single());
+            while (temp.ElementAt(0) == temp.ElementAt(1))
+            {
+                temp.RemoveAt(1);
+                num = gen.Next(range);
+                temp.Add(_db.MorbidityGroups.Where(x => x.Name == ("MorbidityGroup " + num)).Single());
+            }
+            return temp;
+        }
+
+        private DateTime generateDateOfBirth()
+        {
+            Random gen = new Random();
+            DateTime start = new DateTime(1950, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+
+        private DateTime generateDateOfOccurance()
+        {
+            Random gen = new Random();
+            DateTime start = new DateTime(2020, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+
     }
 }
+
+
---
+++ Services/FetchServices.cs
@@ -0,0 +1,29 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Services
+{
+    public class FetchServices
+    {
+        public MyAppContext db { get; set; }
+
+        public List<Patient> getPatientsOlderThan(int age)
+        {
+            //int date = DateTime.Now.Year - age;
+            DateTime date = new DateTime(DateTime.Now.Year - age, DateTime.Now.Month, DateTime.Now.Day);
+            return db.Patients.Where(x => x.Dob < date).ToList();
+        }
+
+        public List<SymptomInstance> getSymptomInstancesByPatientID(long patientID)
+        {
+            var patient = db.Patients.Where(x => x.Id == patientID).Single();
+            return db.SymptomInstances.Where(x => x.Patient == patient).ToList();
+        }
+
+
+    }
+}
